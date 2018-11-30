03f40737e810d216a7a284c19215a9ce34ff971e
alchemistsdaughter <whitehouseanna51@gmail.com>
2018-11-30 16:40:31 -0500

    New Stuff

--- rprogrammingAssignment3-data/rankall.R
+++ rprogrammingAssignment3-data/rankall.R
@@ -24,15 +24,17 @@ rankall <- function(outcome, num = "best") {
   y <- as.numeric(data[,outcome_col])
   ## create a dataframe with state, hospital, and outcome rate
   newdata <- data.frame(data$State, data$Hospital.Name, y)
-  ordered_data <- newdata[order(newdata[,3]),] #order by outcome
-  splitdata <- split(y, ordered_data[,1])
+  newdata <- newdata[order(newdata[,1], newdata[,3], newdata[,2]),] #order by outcome
+  newdata <- na.omit(newdata) #get rid of NAs
+  splitdata <- split(newdata, newdata[,1]) #split data by state
   
-
   ## for each state return the hospital with given rank (num)
-  for (s in length(splitdata)) {
-
+  result <- c()
+  for (s in 1:length(splitdata)) {
+    result[s] <- c(newdata[num,2])
   }
-
+  
+  result
   # OLD CODE: statesub <- subset(data, data$State== state, select = c(2, outcome_col))
   
   ## Return hospital name in that state with the given rank
